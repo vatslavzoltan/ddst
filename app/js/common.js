1f0ee0c07f5bb1b5e3b7c212184979cab381481a
Alex huts <vatslav.zoltan@gmail.com>
2017-11-24 10:57:07 +0200

    24/11

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,24 @@
 $(function() {
 
-	// Custom JS
-
+	//Menu btn
+	$('.nav_icon').click(function(){
+		$(this).toggleClass('open');
+		});
+	$('.nav_item a').click(function () {
+		$('.top_menu').removeClass('active');
+		$('.nav_icon').removeClass('open');
+	}).append('<span>');
+	$(".toggle_menu").click(function () {
+		if($(".top_menu").is(":visible")){
+			$('.top_menu').removeClass('active');
+			$('.top_menu li a').removeClass('fadeInUp animated');
+		} else {
+			$('.top_menu').addClass('active');
+			$('.top_menu li a').addClass('fadeInUp animated');
+		}
+		$('.top_menu').click(function () {
+			$(this).removeClass('active');
+			$('.nav_icon').removeClass('open');
+		});
+	});
 });
--- gulpfile.js
+++ gulpfile.js
@@ -28,6 +28,7 @@ gulp.task('common-js', function() {
 gulp.task('js', ['common-js'], function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/animate/animate-css.js',
 		'app/js/common.min.js', // Всегда в конце
 		])
 	.pipe(concat('scripts.min.js'))
